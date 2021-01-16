b56033c6ba9b207775ed6abe4c268acc4b153325
developer-DV <64646648+developer-DV@users.noreply.github.com>
2021-01-17 00:21:41 +0200

    app initialization

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <header className="header">
+        Online-shop
+      </header>
+    </div>
+  );
+}
+
+export default App;
